d86435c37b7a568eb71ee0edc7b8f377ead3823e
nitesh-05 <niteshgts05@gmail.com>
2025-11-18 13:56:57 +0530

    Analytic added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         <h1>Main Header</h1>
         {children}
+        <Analytics />
         <h1>Footer</h1>
       </body>
     </html>
